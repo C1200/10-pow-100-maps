a8cdb640051b46b7c5ae887159ad128c8560534d
C1200 <40738690+C1200@users.noreply.github.com>
2026-07-11 22:48:49 +0100

    fix tile url format

--- src/Map.tsx
+++ src/Map.tsx
@@ -22,7 +22,7 @@ function Map() {
       attributionControl={false}
     >
       <TileLayer
-        url="https://julsen-bluemap.c1200.workers.dev/maps/world/tiles/x{x}z{y}.png"
+        url="https://julsen-bluemap.c1200.workers.dev/maps/world/tiles/1/x{x}/z{y}.png"
         tileSize={500}
         minZoom={-3}
         maxZoom={2}
